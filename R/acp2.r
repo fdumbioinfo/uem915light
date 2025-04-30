5b066325ae839e46807b6ce36da550233dddc3f1
fdumbioinfo <florent.dumont.bioinfo@gmail.com>
2025-04-30 15:06:38 +0200

    Create acp2.r

---
+++ R/acp2.r
@@ -0,0 +1,80 @@
+#' @title Principal Component Analysis
+#' @description Principal Component Analysis
+#' @param dat matrix numeric
+#' @param factor factor
+#' @param samplename character
+#' @param pc1 numeric
+#' @param pc2 numeric
+#' @param center logical TRUE
+#' @param scale logical TRUE
+#' @param title character
+#' @param legendtitle character
+#' @return no values
+#' @examples
+#' # not run
+#' # acp( mat1 , sif1 )
+#' @author Florent Dumont <florent.dumont@univresite-paris-saclay.fr>
+#' @importFrom magrittr %>%
+#' @import ggplot2
+#' @importFrom stats prcomp setNames
+#' @importFrom scales percent
+#' @export
+acp2 <- function(
+  dat , factor = NULL , samplename = NULL,
+  pc1 = 1 , pc2 = 2,
+  center = TRUE , scale = TRUE,
+  title = "ACP" , legendtitle = "TREATMENT" )
+{
+  ### data
+  if( !is.null( samplename ) ){ dat %>% setNames( samplename ) -> dat }
+  dat %>% as.matrix %>% t %>% prcomp( center = center , scale. = scale ) -> pca0
+  pca0$x %>% data.frame -> pca1
+  pca1 %>% colnames
+  prop <- pca0$sdev^2/sum(pca0$sdev^2)
+  ### plot
+  if( is.null( factor )  )
+  {
+    pca1 %>% ggplot( aes_string( x = paste("PC", pc1 , sep = "") , y = paste("PC", pc2 , sep = "") ) ) -> p
+    p + geom_point( aes( ) , size = 5 ) -> p
+    p + geom_text( label = colnames( dat ) , size = 2, hjust = 0, vjust = 3 , color = "black") -> p
+    p + labs(
+      x = paste( "PC" , pc1 , "(" , percent(prop[pc1]) , ")", sep = "" ),
+      y = paste( "PC" , pc2 , "(" , percent( prop[pc2]) , ")", sep = "") ) -> p
+    p + guides(
+      color = guide_legend( legendtitle ),
+      shape = guide_legend( legendtitle ) ) -> p
+    p + ggtitle( title ) -> p
+    p + theme(
+      plot.title = element_text( size = 12 , face = "bold", hjust = 0.5 ),
+      legend.position = 'right' ) -> p
+    p
+  }else
+    {
+      pca1 %>%
+        ggplot(
+          aes_string(
+            x = paste("PC", pc1 , sep = ""),
+            y = paste("PC", pc2 , sep = "") ) ) -> p
+
+      p + geom_point( aes( color = factor ) , size = 5 ) -> p
+      p + geom_text(
+        label = colnames( dat ),
+        size = 2, hjust = 0, vjust = 3 , color = "black") -> p
+      p + labs(
+        x = paste( "PC" , pc1 , "(" , percent(prop[pc1]) , ")", sep = "" ),
+        y = paste( "PC" , pc2 , "(" , percent( prop[pc2]) , ")", sep = "") ) -> p
+      p + guides(
+        color = guide_legend( legendtitle ),
+        shape = guide_legend( legendtitle ) ) -> p #to change legend name
+      p + ggtitle( title ) -> p
+      p + theme(
+        plot.title = element_text( size = 12 , face = "bold", hjust = 0.5 ),
+        legend.position = 'right',
+        panel.background = element_rect( colour = "black" , fill = "white"),
+        panel.grid.major.x = element_line( colour = "black", linewidth = 0.1 , linetype = "dashed"),
+        panel.grid.major.y = element_line( colour = "black" , linewidth = 0.1, linetype = "dashed" )
+        ) -> p
+      p + scale_color_manual( values = palette0 ) -> p
+      p
+    }
+}
